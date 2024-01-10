985f1c249bc75b00d8626ff8d492fc6b19f5d773
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2024-01-10 09:10:46 +0000

    01_JS

---
+++ 01_JS/01_basic.js
@@ -0,0 +1,10 @@
+let name = "dansih";
+const age = 18;
+var city = "lahore123";
+country = "pakistan";
+let state;
+
+// dont use var kyonke ye block scope or function scope mein kam nahi karta
+// console.table([]); for multiple variable printing in a tabular structure
+
+console.table([name, age, city, country, state]);
---
+++ 01_JS/02_datatypes.js
@@ -0,0 +1,4 @@
+"use strict"; // treat all code as new version of javascript//
+// ek dafa "use strict"; likhne ke baad koi trika nahi h use no strict 
+// lekin javascript outomatic hi code ko newer version ki tarah hi treat karta h ye likhne ki zaroori bhi nahi
+
