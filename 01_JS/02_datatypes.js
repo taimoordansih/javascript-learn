bf5441ef7bc0fffe051e8267a5dcc82cc890bf5e
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2024-01-10 12:50:39 +0000

    01_JS

--- 01_JS/02_datatypes.js
+++ 01_JS/02_datatypes.js
@@ -2,3 +2,10 @@
 // ek dafa "use strict"; likhne ke baad koi trika nahi h use no strict 
 // lekin javascript outomatic hi code ko newer version ki tarah hi treat karta h ye likhne ki zaroori bhi nahi
 
+// -------------alert( 3 + 3 ) we are using node enviroment not broweser------------------
+
+console.log( 3
+    +
+    3 ) // code readability should be high
+    
+console.log("danish")
