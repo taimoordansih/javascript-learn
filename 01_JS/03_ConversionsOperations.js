194262411a21456ac2296b9103c530870ec5adb3
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2024-01-11 13:18:11 +0000

    01_JS

---
+++ 01_JS/03_ConversionsOperations.js
@@ -0,0 +1,29 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let scorre = "34" 
+
+console.log(typeof scorre);
+console.log(typeof (scorre));
+
+let scorrre = "35abc"
+
+console.log(typeof scorrre);
+console.log(typeof (scorrre));
+/*****************CONVERSION********************    */
+
+let valueInNumber = Number(scorrre)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let scorrrre = null
+let valueInNumberr = Number(scorrrre)
+console.log(valueInNumberr);
+console.log(typeof scorrrre);
+
+let scure = undefined
+let valInNumber = Number(scure)
+console.log(typeof scure);
+console.log(valInNumber);
