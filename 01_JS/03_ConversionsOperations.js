d06eb65ebff11f8a05095c33fa4ea3fe5c4e04de
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2024-01-13 09:00:27 +0000

    03_ConversionsOperations

--- 01_JS/03_ConversionsOperations.js
+++ 01_JS/03_ConversionsOperations.js
@@ -1,58 +1,110 @@
-let score = 33
+// let score = 33
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let scorre = "34" 
+// let scorre = "34" 
 
-console.log(typeof scorre);
-console.log(typeof (scorre));
+// console.log(typeof scorre);
+// console.log(typeof (scorre));
 
-let scorrre = "35abc"
+// let scorrre = "35abc"
 
-console.log(typeof scorrre);
-console.log(typeof (scorrre));
-/*****************CONVERSION********************    */
+// console.log(typeof scorrre);
+// console.log(typeof (scorrre));
+// /*****************CONVERSION********************    */
 
-let valueInNumber = Number(scorrre)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(scorrre)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
-let scorrrre = null
-let valueInNumberr = Number(scorrrre)
-console.log(valueInNumberr);
-console.log(typeof scorrrre);
+// let scorrrre = null
+// let valueInNumberr = Number(scorrrre)
+// console.log(valueInNumberr);
+// console.log(typeof scorrrre);
 
-let scure = undefined
-let valInNumber = Number(scure)
-console.log(typeof scure);
-console.log(valInNumber);
+// let scure = undefined
+// let valInNumber = Number(scure)
+// console.log(typeof scure);
+// console.log(valInNumber);
 
-let code = 1
-let numInBool = Boolean(code)
-console.log(typeof (numInBool));
-console.log(numInBool);
+// let code = 1
+// let numInBool = Boolean(code)
+// console.log(typeof (numInBool));
+// console.log(numInBool);
 
-let coder = 0
-let numInBoole = Boolean(coder)
-console.log(typeof (numInBoole));
-console.log(numInBoole);
+// let coder = 0
+// let numInBoole = Boolean(coder)
+// console.log(typeof (numInBoole));
+// console.log(numInBoole);
 
-// 1 = true , 0 = false
-// "" = false                   empty string = false
-// "danish" = true               string = true
+// // 1 = true , 0 = false
+// // "" = false                   empty string = false
+// // "danish" = true               string = true
 
-let nam = ""
-let sInBol = Boolean(nam)
-console.log(typeof (sInBol));
-console.log(sInBol); 
+// let nam = ""
+// let sInBol = Boolean(nam)
+// console.log(typeof (sInBol));
+// console.log(sInBol); 
 
-let naam = "danish"
-let sInBole = Boolean(naam)
-console.log(typeof (sInBole));
-console.log(sInBole);
+// let naam = "danish"
+// let sInBole = Boolean(naam)
+// console.log(typeof (sInBole));
+// console.log(sInBole);
 
-let aNum = 33
-let NumToString = String(aNum)
-console.log(typeof (NumToString));
-console.log(NumToString);
+// let aNum = 33
+// let NumToString = String(aNum)
+// console.log(typeof (NumToString));
+// console.log(NumToString);
+
+//******************************Conversions**************************************
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 power 3
+// console.log(2/2);
+// console.log(2%2);  // Remanider
+
+// plus two stings
+
+let str1 = "Hello"
+let str2 = " Danish"
+let str3 = str1 + str2
+console.log(str3);
+
+// plus string to number
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+// use () to impliment operation first
+console.log(( 3 + 3 ) * 5 / 2); 
+
+// ****************Complex Conversions Dno't Use theme*************************
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+console.log(-true);
+console.log(-"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+// *************prefix - postfix*****************
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+let gamCounter = 100
+gamCounter++;
+console.log(gamCounter);
+
+// *******************************************************************************************
