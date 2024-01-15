6db9485516128e39c3d526210ae79a3a58ede50d
taimoordansih <147491120+taimoordansih@users.noreply.github.com>
2024-01-15 12:05:54 +0000

    04_compersions

---
+++ 01_JS/04_Comperisions.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2"> 1);
+console.log("02"> 1);
+
+console.log(null>0);
+console.log(null<0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null==0);
+//= or <,> >=,<= different trike se kam karte hein < in mein null ko 0 mein covert kardiya jata h lekin dosre mein nahi
+// undefine mein sare operators mein false hi dikhata h
